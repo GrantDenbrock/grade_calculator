ee997a69ae136aa847f3402766cc21b369509222
Grant Denbrock <gdenbroc@asu.edu>
2016-08-02 21:40:51 -0700

    initial commit

---
+++ grade_calculator.py
@@ -0,0 +1,29 @@
+#grade_calculator_2.py
+class Grades:
+	Weights = {}
+	Exam = []
+	Homework = []
+	add_Homework(earned,possible)
+	add_Exam(earned,possible)
+	report_scores()
+	
+object Homework:
+	Name = None
+	Possible = int
+	Earned = int
+	Description = None
+	
+object Exam:
+	Name = None
+	Possible = int
+	Earned = int
+	Description = None
+	
+# creating: add_homework() #
+def add_Homework(Earned,Possible,Name=None,Description=None):
+	self.Homework.append(Homework(Earned,Possible,Name=Name,Description=Description))
+	
+
+	
+
+	
